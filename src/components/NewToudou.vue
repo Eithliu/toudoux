f2059f0a4539100bd54fccf8cf51fcfb6df2e754
Eithliu <iris.benoit@pix.fr>
2024-10-23 14:04:36 +0200

    WIP: redesign app according to iOS design

--- src/components/NewToudou.vue
+++ src/components/NewToudou.vue
@@ -15,16 +15,25 @@ async function createNewToudou() {
   });
   router!.navigate('toudou-details', { id });
 }
+
+function goBack() {
+  router!.navigate('new-toudou');
+}
 </script>
 
 <template>
 <form class="form">
-  <label for="toudouTitle">
-    Choisis le titre de ta Toudou
-  </label>
-  <input id="toudouTitle" v-model="title" class="title-user" />
-  <input type="color" v-model="color" />
-  <button @click="createNewToudou">Ok !</button>
+  <div class="actions">
+    <button @click="goBack">Annuler</button>
+    <button @click="createNewToudou">Ok</button>
+  </div>
+  <div class="card">
+    <label for="toudouTitle" class="hidden">
+      Choisis le titre de ta Toudou
+    </label>
+    <input id="toudouTitle" v-model="title" class="title-user" placeholder="Nom de la toudou" />
+  </div>
+  <input type="color" v-model="color" class="color-picker" />
 </form>
 </template>
 
@@ -33,10 +42,42 @@ async function createNewToudou() {
   display: flex;
   flex-direction: column;
   width: 100%;
-  align-items: center;
   gap: 10px;
+  background: white;
+  margin: 1rem;
+  padding: 1rem;
+  border-radius: 10px;
+  box-shadow: 0 0 5px #b9b9b9;
+  align-items: center;
 }
 .form > button, .title-user {
   width: 100%;
+}
+input.title-user {
+  background: #f1f1f1;
+  border: none;
+  border-radius: 0;
+  font-size: 15px;
+  text-align: center;
+  padding: .5rem 0;
+
+}
+.actions {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+ }
+.actions > button {
+  font-size: 15px;
+}
+.card {
+  background: white;
+  border-radius: 5px;
+  margin: 1rem;
+  width: 100%;
+}
+button {
+ padding: 0;
 }
 </style>
--- src/components/TitlePage.vue
+++ src/components/TitlePage.vue
@@ -17,11 +17,17 @@ function goToToudouDetails(id: string) {
 
 <template>
   <div class="wrapper">
-    <h1>My Toudoux</h1>
-    <ul v-for="toudou in toudoux">
-      <li :style="`color: ${toudou.color}`" @click="goToToudouDetails(toudou.id)">{{toudou.title}}</li>
-    </ul>
-    <button @click="goToNewToudou()">Créer une toudoux</button>
+    <main class="main">
+      <h1>My Toudoux</h1>
+      <div class="card">
+        <ul v-for="toudou in toudoux">
+          <li :style="`color: ${toudou.color}`" @click="goToToudouDetails(toudou.id)">{{toudou.title}}</li>
+        </ul>
+      </div>
+    </main>
+    <footer>
+      <button @click="goToNewToudou()" class="new-toudou-btn"><span class="plus">+</span> Créer une toudoux</button>
+    </footer>
   </div>
 </template>
 
@@ -29,6 +35,29 @@ function goToToudouDetails(id: string) {
 .wrapper {
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
   color: #54577C;
+  width: 100%;
+  min-height: 85vh;
+}
+.main {
+  display: flex;
+  flex-direction: column;
+
+}
+.card {
+  background: white;
+  margin: 1rem;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #d3d3d3;
+}
+footer {
+  display: flex;
+  flex-direction: row-reverse;
+  margin-right: 1rem;
+}
+.new-toudou-btn {
+  font-size: 15px;
 }
 </style>
